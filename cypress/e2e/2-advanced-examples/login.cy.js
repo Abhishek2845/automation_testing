b92eadf83e8f88d2bfd3ef1790d8a1d30a7c51f3
unknown <Abhishek Yadav>
2022-09-10 15:26:20 +0530

    First commit

---
+++ cypress/e2e/2-advanced-examples/login.cy.js
@@ -0,0 +1,63 @@
+it('login_page!', function () {
+
+    cy.visit('https://radxup-colectiv-uat.azurewebsites.net/participant-login')
+
+    cy.title().should('eq','Home | RADxUP.')
+
+    cy.wait(2000)
+
+    cy.get(".participant-signin").click()
+
+    cy.wait(2000)
+
+    cy.get('input[value=English]').should('be.visible').should('be.checked')
+
+    cy.wait(2000)
+
+    //cy.get('input[Value=Spanish').Should('be.visible').should('not.be.checked').click()
+
+    cy.wait(2000)
+
+    cy.get('button').click()
+
+    cy.wait(2000)
+
+    cy.get('input[name=email]').should('be.visible').should('be.enabled').type("jack@yopmail.com")
+
+    cy.wait(2000)
+
+    cy.get('input[name=password]').should('be.visible').should('be.enabled').type("Test@123")
+
+    cy.wait(2000)
+
+    cy.get('button').click()
+
+  })
+
+describe('lpogin_page', function () {
+
+  it('participant_screen', function () {
+
+    cy.wait(2000)
+
+   
+
+    cy.wait(2000)
+
+    cy.visit('https://radxup-colectiv-uat.azurewebsites.net/study-brief')
+
+    cy.wait(3000)
+
+    cy.visit('https://radxup-colectiv-uat.azurewebsites.net/current-forms')
+
+    cy.wait(3000)
+
+     cy.visit('https://radxup-colectiv-uat.azurewebsites.net/complete-forms')
+
+    cy.wait(3000)
+
+    cy.get("https://radxup-colectiv-uat.azurewebsites.net/invitations").click()
+
+
+})
+})
